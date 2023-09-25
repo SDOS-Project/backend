71baaddb7c677765ad058fd45ae6bcf8f67f473f
aryanteng <aryanteng892@gmail.com>
2023-09-25 20:15:33 +0530

    added firebase auth, preauth middleware for firebase token

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectModule } from './project/project.module';
+import { FirebaseApp } from './firebase/firebase-service';
+import { PreAuthMiddleware } from './middleware/auth.middleware';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ProjectModule],
+  imports: [ProjectModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, FirebaseApp],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(PreAuthMiddleware).exclude('/auth*').forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
