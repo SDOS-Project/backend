84d7cc5de1b09d2fd1b25cb330a4cf8e60f30aa7
aryanteng <aryanteng892@gmail.com>
2023-09-30 20:31:43 +0530

    signup dto

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,12 +1,52 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { UserRole } from '@prisma/client';
+import {
+  IsArray,
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator';
 
 export class SignUpDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   firstName: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   lastName: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
   email: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   password: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsEnum(UserRole)
   role: UserRole;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   organisationHandle: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsArray()
   areasOfInterest: string[];
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   firebaseId: string;
 }
