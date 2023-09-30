c544d811710743b83f4cfffb718996a4f78cc372
aryanteng <aryanteng892@gmail.com>
2023-09-30 19:36:25 +0530

    global validation pipe

--- dist/main.js
+++ dist/main.js
@@ -4,8 +4,10 @@ const core_1 = require("@nestjs/core");
 const swagger_1 = require("@nestjs/swagger");
 const app_module_1 = require("./app.module");
 const helmet_1 = require("helmet");
+const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true });
+    app.useGlobalPipes(new common_1.ValidationPipe());
     app.use((0, helmet_1.default)());
     const config = new swagger_1.DocumentBuilder()
         .setTitle('SDOS APIs')
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-
+  app.useGlobalPipes(new ValidationPipe());
   app.use(helmet());
-
   const config = new DocumentBuilder()
     .setTitle('SDOS APIs')
     .setDescription('The SDOS API description')
