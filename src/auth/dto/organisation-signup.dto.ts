39d1645d042b53d5f2e88f162aa69f568d0365d4
aryanteng <aryanteng892@gmail.com>
2023-09-30 19:52:31 +0530

    organisation signup dto

--- src/auth/dto/organisation-signup.dto.ts
+++ src/auth/dto/organisation-signup.dto.ts
@@ -1,12 +1,45 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { OrganisationType } from '@prisma/client';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class OrganisationSignUpDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   email: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   password: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   type: OrganisationType;
+
+  @ApiProperty()
+  // @IsNotEmpty()
+  @IsString()
   logoUrl: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   address: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   ipPolicy: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   firebaseId: string;
 }
