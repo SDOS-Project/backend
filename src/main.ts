7472c7107bce3afef98bb9e00c555394c1fbdca9
aryanteng <aryanteng892@gmail.com>
2023-11-11 18:53:07 +0530

    added https options

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const httpsOptions = {
+    key: fs.readFileSync('./private-key.pem'),
+    cert: fs.readFileSync('./public-cert.pem'),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions, cors: true });
   app.useGlobalPipes(new ValidationPipe());
   app.use(helmet());
   const config = new DocumentBuilder()
