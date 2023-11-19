819517fd6b920975d6ad1e6e91912d5a9c9beab1
aryanteng <aryanteng892@gmail.com>
2023-11-20 00:09:55 +0530

    http module in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [PrismaModule],
+  imports: [PrismaModule, HttpModule],
 })
 export class AuthModule {}
