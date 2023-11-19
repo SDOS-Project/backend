64a541e6b90855609f082743826b2f22b89314c6
aryanteng <aryanteng892@gmail.com>
2023-11-19 22:16:55 +0530

    created student signup dto

---
+++ src/auth/dto/student.signup.dto.ts
@@ -0,0 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class StudentSignupDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  firstName: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  lastName: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  firebaseId: string;
+}
