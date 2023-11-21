104a5bb1914735904fc3592fb9befefc98edfa1a
aryanteng <aryanteng892@gmail.com>
2023-11-21 19:40:13 +0530

    update user workflow

--- src/user/mock/index.ts
+++ src/user/mock/index.ts
@@ -1,7 +1,9 @@
+import { Discipline } from '@prisma/client';
 import { UpdateUserDto } from '../dto/update-user.dto';
 
 export const mockUpdateUserDto: UpdateUserDto = {
   firstName: 'John',
   lastName: 'Doe',
+  discipline: Discipline.AerospaceEngineering,
   areasOfInterest: ['Area 1', 'Area 2'],
 };
